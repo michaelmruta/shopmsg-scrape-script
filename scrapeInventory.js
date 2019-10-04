eaea570d10817747cf15b134c0767e2324058b50
Meku <michaelmruta@users.noreply.github.com>
2019-10-04 18:53:14 +0800

    Converted to ES6

--- scrapeInventory.js
+++ scrapeInventory.js
@@ -1,95 +1,44 @@
 #!/usr/local/bin/node
+const { Parser } = require('json2csv'),
+    fs = require('fs'),
+    fetch = require('node-fetch'),
+    args = require('args');
 
-/**
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Author: Michael Angelo Ruta (2019)
- *
- **/
-
-const { Parser } = require('json2csv');
-const fs = require('fs');
-const request = require('request-json');
-const args = require('args');
-
-const fields = ['product_id',
-    'product_title',
-    'variant_id',
-    'variant_title',
-    'variant_price',
-    'inventory_quantity',
-    'captured_timestamp'
-]
-
+// args
 args.option('file', 'Outpuf filename', 'output.csv')
     .option('site', 'URL of the target site', 'www.puravidabracelets.com')
-    .option('page', 'Pages to scrape', Infinity)
-
+    .option('page', 'Pages to scrape', 5)
 const flags = args.parse(process.argv)
-const client = request.createClient(`https://${flags.site}`);
-const parser = new Parser({ header: false, fields: fields });
 
+// csv
+const fields = ['product_id', 'product_title', 'variant_id', 'variant_title', 'variant_price', 'inventory_quantity', 'captured_timestamp']
+const parser = new Parser({ header: false, fields: fields });
 fs.writeFileSync(flags.file, fields.join(",") + "\n");
 
-function writeToCSV(json) {
-    try {
-        var csv = parser.parse(json);
-        fs.appendFileSync(flags.file, csv + "\n")
-    } catch (err) {
-        console.error(err);
-        process.exit(0);
+// requests
+(async () => {
+    for (let page = 1; page < flags.page; page++) {
+        let resp = await fetch(`https://${flags.site}/products.json?page=${page}`)
+        let json = await resp.json()
+
+        for (product of json.products) {
+            let resp = await fetch(`https://${flags.site}/products/${product.handle}.json`)
+            let json_ = await resp.json()
+
+            for (variant of json_.product.variants) {
+                let json = {
+                    product_id: product.id,
+                    product_title: product.title,
+                    variant_id: variant.id,
+                    variant_title: variant.title,
+                    variant_price: variant.price,
+                    inventory_quantity: variant.inventory_quantity,
+                    captured_timestamp: new Date()
+                }
+                console.log(json)
+                fs.appendFileSync(flags.file, parser.parse(json) + "\n")
+            }
+
+        }
     }
-}
-
-function getProductDetails(products) {
-        var product = products.shift();
-        if(product) {
-            console.log(flags.site + `/products/${product.handle}.json`)
-            client.get(`/products/${product.handle}.json`, function(err, res, body) {
-                if (err) {
-                    console.error(err);
-                    process.exit(0);
-                }
-                var product = body.product;
-                for (var j in product.variants) {
-                    writeToCSV({
-                        product_id: product.id,
-                        product_title: product.title,
-                        variant_id: product.variants[j].id,
-                        variant_title: product.variants[j].title,
-                        variant_price: product.variants[j].price,
-                        inventory_quantity: product.variants[j].inventory_quantity,
-                        captured_timestamp: new Date()
-                    })
-                }
-                getProductDetails(products)
-            });
-        }
-}
-
-function getProducts(page = 1) {
-    console.log(flags.site + '/products.json?page=' + page)
-    client.get('/products.json?page=' + page, function(err, res, body) {
-        if (err) {
-            console.error(err);
-            process.exit(0);
-        }
-        getProductDetails(body.products)
-        if (page < flags.page) {
-            getProducts(++page)
-        }
-    })
-}
-
-getProducts();
+})();
